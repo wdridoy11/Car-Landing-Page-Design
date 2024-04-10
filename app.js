0575fd3392886bedb42a96ef87b8b48f27b06328
Ridoy sharif <wdridoy11@gmail.com>
2024-04-10 19:06:30 +0600

    gallery testimonial and footer section design complete

---
+++ app.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    loop: true,
+    initialSlide: 1,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1200: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        }
+    },
+
+
+});
